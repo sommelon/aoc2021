cb1e4b935d0fab36cc04ef90a0c5a1fd3b7a3216
Simeon Králik <simeon.kralik@gmail.com>
2021-12-02 12:58:12 +0100

    Updated puzzle 1-1

--- 1/1_ref.py
+++ 1/1_ref.py
@@ -1,6 +1,4 @@
 file = open('./input.txt', 'r')
-lines = file.readlines()
-
-lines = list(map(int, lines))
+lines = list(map(int, file.readlines()))
 increased_count = sum(1 for i in range(1, len(lines)) if lines[i] > lines[i-1])
 print(increased_count) #1688
