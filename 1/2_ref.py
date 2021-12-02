d5c52d80b527b9ea86f9c0aaf7270378d18d0c01
Simeon Králik <simeon.kralik@gmail.com>
2021-12-02 12:58:20 +0100

    Updated puzzle 1-2

--- 1/2_ref.py
+++ 1/2_ref.py
@@ -1,9 +1,7 @@
 import itertools
 
 file = open('./input.txt', 'r')
-lines = file.readlines()
-
-lines = list(map(int, lines))
+lines = list(map(int, file.readlines()))
 sliding_windows = itertools.zip_longest(lines, lines[1:], lines[2:], fillvalue=0)
 window_sums = list(map(sum, sliding_windows))
 increased_count = sum(1 for i in range(1, len(window_sums)) if window_sums[i] > window_sums[i - 1])
