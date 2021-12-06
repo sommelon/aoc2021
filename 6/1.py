f908be49e89748f6859a20af9a00d64afc9c4178
Simeon Králik <simeon.kralik@gmail.com>
2021-12-06 17:27:42 +0100

    Solved puzzle 6-1

---
+++ 6/1.py
@@ -0,0 +1,13 @@
+file = open('./sample.txt', 'r')
+timers = [int(num) for num in file.read().split(',')]
+file.close()
+
+for day in range(80):
+    for i in range(len(timers)):
+        if timers[i] == 0:
+            timers[i] = 6
+            timers.append(8)
+        else:
+            timers[i] -= 1
+
+print(len(timers))
